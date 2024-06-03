8cebfeffb548e1a3035fb4124a9847a1f56cd5cc
FatimaNadeem831 <167708955+FatimaNadeem831@users.noreply.github.com>
2024-06-03 07:05:21 -0700

    project 2 cli number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to Fatima - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(number limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulation ! you guess a correct number.");
+}
+else {
+    console.log("Sorry , you guess a wrong number");
+}
